ae35bb6c50f1080426b9de346dc4312b18de4b0b
sohamart <sohamduttabwn@gmail.com>
2026-07-07 13:06:19 +0530

    frontend add home page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,20 +8,26 @@ import MainLayout from './/Layout/MainLayout'
 
 const App = () => {
   useEffect(() => {
-    const lenis = new Lenis({
-      duration: 1.2,
-      smoothWheel: true,
-    });
+  const lenis = new Lenis({
+    duration: 1.4,
+    smoothWheel: true,
+    syncTouch: true,       // Mobile support
+    syncTouchLerp: 0.08,
+    touchMultiplier: 1.2,
+    wheelMultiplier: 1,
+  });
 
-    function raf(time) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-
+  function raf(time) {
+    lenis.raf(time);
     requestAnimationFrame(raf);
+  }
 
-    return () => lenis.destroy();
-  }, []);
+  requestAnimationFrame(raf);
+
+  return () => {
+    lenis.destroy();
+  };
+}, []);
   return (
     <div className=" flex flex-col w-full   bg-black no-scrollbar" >
 
