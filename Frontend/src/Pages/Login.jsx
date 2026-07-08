27ca56a981b55d9d15a04a8053c879c152db3784
sohamart <sohamduttabwn@gmail.com>
2026-07-08 14:23:32 +0530

    frontend add home page

--- Frontend/src/Pages/Login.jsx
+++ Frontend/src/Pages/Login.jsx
@@ -11,8 +11,8 @@ const Login = () => {
     <div className="min-h-screen bg-black flex items-center justify-center px-4  overflow-hidden relative">
 
       {/* Background Glow */}
-      <div className="absolute left-0 top-0 w-96 h-96 bg-orange-500/20 blur-[140px]" />
-      <div className="absolute right-0 bottom-0 w-96 h-96 bg-orange-500/20 blur-[140px]" />
+      <div className="absolute  left-0 top-0 w-96 h-96 lg:bg-orange-500/20 bg-orange-500/5 blur-[140px]" />
+      <div className="absolute right-0 bottom-0 w-96 h-96 lg:bg-orange-500/20 bg-orange-500/5 blur-[140px]" />
 
       <div className="relative lg:mt-25 mb-5 lg:mb-0 mt-30 z-10 w-full max-w-5xl rounded-3xl overflow-hidden border border-orange-500/20 bg-white/[0.04] backdrop-blur-3xl">
 
