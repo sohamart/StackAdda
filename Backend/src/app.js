44d95728e4a030dfed2a3fb55a8b809c1e308cd8
sohamart <sohamduttabwn@gmail.com>
2026-07-08 14:06:43 +0530

    frontend add home page

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,7 +13,8 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ['http://localhost:3000', "http://localhost:5173", "https://stack-adda.vercel.app", ],
+  
     credentials: true,
   })
 );
