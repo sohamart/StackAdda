acdce9739d02ec4fc225a7ae09b2fb1bdbe9b818
sohamart <sohamduttabwn@gmail.com>
2026-07-08 21:16:18 +0530

    deploy

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URI,
-  withCredentials: true, 
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
 });
 
 export default API;
