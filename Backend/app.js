1eaafa1d627a5bab9177a10dd613cef3609e4641
sohamart <sohamduttabwn@gmail.com>
2026-07-09 15:38:14 +0530

    deploy

--- Backend/app.js
+++ Backend/app.js
@@ -30,12 +30,12 @@ app.use(cookieParser());
 
 
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: ['http://localhost:3000', "http://localhost:5173", "https://stack-adda.vercel.app/", "https://stackbackend-omega.vercel.app/"],
+  
+    credentials: true
+},
+));
 
 
 app.use("/api/auth", authRoutes);
