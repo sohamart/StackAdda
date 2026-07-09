7c04b2ac7c80c0d892bc89e321bb6597e00b8cdf
Soham Dutta <sohamduttabwn@gmail.com>
2026-07-09 16:45:33 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -31,7 +31,7 @@ app.use(cookieParser());
 
 
 app.use(cors({
-  origin: ['http://localhost:3000', "http://localhost:5173", "https://stack-adda.vercel.app/", "https://stackbackend-omega.vercel.app/"],
+  origin: ['http://localhost:3000', "http://localhost:5173", "https://stack-adda.vercel.app", "https://stackbackend-omega.vercel.app"],
   
     credentials: true
 },
